2ad830f27c5de668327817f4194a6ad8b363b013
Justin Biard <icodealot@gmail.com>
2022-04-12 12:12:07 -0500

    Adding sorted list of keys for routes.

--- mux/mux.go
+++ mux/mux.go
@@ -2,7 +2,9 @@ package mux
 
 import (
 	"net/http"
+	"sort"
 	"strings"
+	"sync"
 )
 
 type RouteEntry struct {
@@ -11,15 +13,24 @@ type RouteEntry struct {
 	method  string
 }
 
+// As per https://go.dev/blog/maps, map order is undefined
+// and not guaranteed to be the same. (a language decision)
+// A sorted slice of string keys in the handler is used to
+// match routes most to least specific pattern.
 type HttpHandler struct {
-	//Keys will be method:route such as...
-	// GET:/items
-	routes map[string]*RouteEntry
+	// Keys will be method:route such as: "GET:/items"
+	// Could also use a struct key such as {string, string}
+	// but keeping it string-based for now.
+	routes map[string]RouteEntry
+
+	mutex sync.RWMutex // sync on Write operations (see addRouteEntry)
+
+	sk []string // sorted slice of keys by length of route
 }
 
 func NewHandler() *HttpHandler {
 	return &HttpHandler{
-		routes: make(map[string]*RouteEntry),
+		routes: make(map[string]RouteEntry),
 	}
 }
 
@@ -43,37 +54,59 @@ func (mux *HttpHandler) OPTIONS(r string, h http.HandlerFunc) {
 	mux.addRouteEntry(r, h, "OPTIONS")
 }
 
+func (mux *HttpHandler) HandleFunc(r string, h http.HandlerFunc) {
+	mux.addRouteEntry(r, h, "*")
+}
+
 func (mux *HttpHandler) addRouteEntry(r string, h http.HandlerFunc, m string) {
 	if len(r) == 0 || h == nil || len(m) == 0 {
 		panic("mux: nil handler or empty route or method")
 	}
-	mux.routes[m+":"+r] = &RouteEntry{
+	mux.mutex.Lock()
+	defer mux.mutex.Unlock()
+	key := m + ":" + r
+	mux.routes[key] = RouteEntry{
 		handler: h,
 		route:   r,
 		method:  m,
 	}
+	mux.sk = append(mux.sk, key)
+	n := len(mux.sk)
+	if n == 1 || len(key) <= len(mux.sk[n-2]) { // already sorted so just return
+		return
+	}
+	sort.Slice(mux.sk, func(i, j int) bool { // readability over speed given a one time hit
+		return len(mux.sk[i]) > len(mux.sk[j]) // sort longest path to shortest
+	})
 }
 
-func (mux *HttpHandler) findRoute(path string) *RouteEntry {
+// Using a length sorted slice for those familiar with default mux
+// and match an exact route first if applicable
+func (mux *HttpHandler) findRoute(path string) http.Handler {
 	// First try to grab the exact route
 	if re, ok := mux.routes[path]; ok {
-		return re
+		return re.handler
 	}
 	// Second find routes with a matching prefix
-	for k, re := range mux.routes {
-		// consider using a length sorted slice similar to Default mux
-		if strings.HasPrefix(path, k) {
-			return re
+	for _, key := range mux.sk {
+		if key[0] == '*' { // catchall so replace method in path with *
+			_, urlPattern, found := strings.Cut(path, ":")
+			if found {
+				path = "*:" + urlPattern
+			}
+		}
+		if strings.HasPrefix(path, key) {
+			return mux.routes[key].handler
 		}
 	}
 	return nil
 }
 
 func (mux *HttpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	route := mux.findRoute(r.Method + ":" + r.URL.Path)
-	if route == nil {
+	handler := mux.findRoute(r.Method + ":" + r.URL.Path)
+	if handler == nil {
 		http.NotFound(w, r)
 		return
 	}
-	route.handler(w, r)
+	handler.ServeHTTP(w, r)
 }
